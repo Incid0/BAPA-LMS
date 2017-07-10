e08d99cca9d652bb1933948ab21d21fa330653ae
Anton <antonolsson91@yahoo.com>
2017-07-10 16:51:27 +0200

    Removed register

---
+++ BAPA-LMS/Scripts/calendarScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultView: 'agendaDay',
+        editable: true,
+        allDaySlot: false,
+        selectable: true,
+        slotMinutes: 15
+
+    });
+});
