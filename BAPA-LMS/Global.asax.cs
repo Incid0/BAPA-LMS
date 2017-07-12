0651c31332db9333e7bf5856085a413062091dfd
Peter Nilsson <incido@gmail.com>
2017-07-12 03:32:49 +0200

    Id Scrambler Unit Test added ModelExtension Id Scrambler added

--- BAPA-LMS/Global.asax.cs
+++ BAPA-LMS/Global.asax.cs
@@ -16,6 +16,8 @@ namespace BAPA_LMS
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			LMSConfig.Init();
         }
     }
 }
