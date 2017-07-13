48f734c4c7e82fe985ddf8954e1ac5c58619f997
Anton <antonolsson91@yahoo.com>
2017-07-13 17:03:30 +0200

    Added basic course name in Indexview

---
+++ BAPA-LMS/Models/CourseViewModels/CourseIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BAPA_LMS.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAPA_LMS.Models.CourseViewModels
+{
+    public class CourseIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static implicit operator CourseIndexViewModel(Course model)
+        {
+            return new CourseIndexViewModel
+            {
+                Name = model.Name
+            };
+        }
+    }
+}
