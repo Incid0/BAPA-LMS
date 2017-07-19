e08c3378cabe4740129873f03a8e4e1df44452cf
Anton <antonolsson91@yahoo.com>
2017-07-19 13:09:23 +0200

    Removed shit

--- BAPA-LMS/Models/DB/Activity.cs
+++ BAPA-LMS/Models/DB/Activity.cs
@@ -20,14 +20,10 @@ namespace BAPA_LMS.Models.DB
 		public DateTime EndTime { get; set; }
 		public int TypeId { get; set; }
 		public int ModuleId { get; set; }
-        public int? MemberId { get; set; }
-        public int? DocumentId { get; set; }
 
         public virtual ActivityType Type { get; set; }
 		public virtual Module Module { get; set; }
-        public virtual ApplicationUser Member { get; set; }
-        public virtual Document Document { get; set; }
-        public virtual ICollection<ApplicationUser> Members { get; set; }
-        public virtual ICollection<Document> Documents { get; set; }
+  
+  
     }
 }
