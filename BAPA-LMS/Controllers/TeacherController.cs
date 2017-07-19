930e5430770270b767e5960a06382c856959156e
Anton <antonolsson91@yahoo.com>
2017-07-19 13:44:17 +0200

    Added teacher

--- BAPA-LMS/Controllers/TeacherController.cs
+++ BAPA-LMS/Controllers/TeacherController.cs
@@ -2,9 +2,9 @@
 using BAPA_LMS.Models;
 using BAPA_LMS.Models.CourseViewModels;
 using BAPA_LMS.Models.DB;
-using BAPA_LMS.Utils;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web;
@@ -16,48 +16,51 @@ using System.Web.Mvc;
 
 namespace BAPA_LMS.Controllers
 {
-    [Authorize(Roles = "Admin")]
+	[Authorize(Roles = "Admin")]
 	public class TeacherController : Controller
-    {
-        private ApplicationSignInManager _signInManager;
-        private ApplicationUserManager _userManager;
+	{
+		private ApplicationSignInManager _signInManager;
+		private ApplicationUserManager _userManager;
 
-        public ApplicationUserManager UserManager
-        {
-            get
-            {
-                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
-        }
-        public ApplicationSignInManager SignInManager
-        {
-            get
-            {
-                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            }
-            private set
-            {
-                _signInManager = value;
-            }
-        }
+		public ApplicationUserManager UserManager
+		{
+			get
+			{
+				return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			}
+			private set
+			{
+				_userManager = value;
+			}
+		}
+		public ApplicationSignInManager SignInManager
+		{
+			get
+			{
+				return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+			}
+			private set
+			{
+				_signInManager = value;
+			}
+		}
 
-        private LMSDbContext db = new LMSDbContext();
+		private LMSDbContext db = new LMSDbContext();
 
-        public ActionResult Index()
-        {
-            var currentUser = UserUtils.GetCurrentUser(HttpContext);
-         
-            Course course = db.Courses.Find(currentUser.CourseId);
-            CourseIndexViewModel cdvm = course;
-            return View(cdvm);
-        }
+		public ActionResult Index()
+		{
+		 List<CourseListViewModel> courseList = new List<CourseListViewModel>();
+			foreach (var item in db.Courses)
+			{
+				courseList.Add(item);
+			}
 
-        public ActionResult CourseEdit(int? id)
-        {
+			return View(courseList);
+		}
+
+
+	public ActionResult CourseEdit(int? id)
+		{
 			if (id == null)
 			{
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -69,62 +72,87 @@ namespace BAPA_LMS.Controllers
 			}
 			CourseIndexViewModel civm = course;
 			return View(civm);
-        }
-        [AllowAnonymous]
-        public ActionResult Register()
-        {
-            return View();
-        }
+		}
+		[AllowAnonymous]
+		public ActionResult RegisterTeacher()
+		{
+			return View();
+		}
 
-        //
-        // POST: /Account/Register
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model, int id)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, CourseId = id, FirstName = model.FirstName, LastName = model.LastName };
-                
-                var result = await UserManager.CreateAsync(user, model.Password);
-             
-                if (result.Succeeded)
-                {
-                    result = UserManager.AddToRole(user.Id, "Member");
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+		//
+		// POST: /Account/Register
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Register(RegisterViewModel model, int id)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser { UserName = model.Email, Email = model.Email, CourseId = id, FirstName = model.FirstName, LastName = model.LastName };
+				
+				var result = await UserManager.CreateAsync(user, model.Password);
+			 
+				if (result.Succeeded)
+				{
+					result = UserManager.AddToRole(user.Id, "Member");
+					await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+					
 
-                    return RedirectToAction("Index", "Student");
-                }
-                AddErrors(result);
-            }
+					return RedirectToAction("Index", "Teacher");
+				}
+				AddErrors(result);
+			}
+		
+			return View(model);
+		}
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> RegisterTeacher(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+				var result = await UserManager.CreateAsync(user, model.Password);
+
+				if (result.Succeeded)
+				{
+					result = UserManager.AddToRole(user.Id, "Admin");
+					await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+					// For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+					// Send an email with this link
+					// string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+					// var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+					// await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+					return RedirectToAction("Index", "Student");
+				}
+				AddErrors(result);
+			}
+
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 
 
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
-        }
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		private void AddErrors(IdentityResult result)
+		{
+			foreach (var error in result.Errors)
+			{
+				ModelState.AddModelError("", error);
+			}
+		}
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-    }
+	}
 }
--- BAPA-LMS/Models/DB/IdentityModels.cs
+++ BAPA-LMS/Models/DB/IdentityModels.cs
@@ -17,7 +17,7 @@ namespace BAPA_LMS.Models.DB
 
         public virtual Course Course { get; set; }
 
-        public virtual ICollection<Document> Documents { get; set; }
+    
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
