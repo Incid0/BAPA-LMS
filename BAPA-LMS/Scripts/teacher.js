c6fefd04d15b932853d913462dd3465b72e20129
Peter Nilsson <incido@gmail.com>
2017-07-20 11:01:55 +0200

    Added an Alertbox system

--- BAPA-LMS/Scripts/teacher.js
+++ BAPA-LMS/Scripts/teacher.js
@@ -1,5 +1,18 @@
 ﻿var teacher = (function ($) {
     var lastModule = 0;
+    var alertbox;
+
+    function localAlert(message, style) {
+        var parent = alertbox.parent();
+        if (message) alertbox.text(message);
+        if (!style) style = 'success';
+        parent.removeClass().addClass('alert alert-' + style);
+        parent.show();
+        setTimeout(function () {
+            parent.fadeOut(500);
+        }, 2000);
+    }
+
     function treeNodeSelect(event, node) {
         var index = node.id, ctrl = index[0], id = index.substr(1);
         // Limit activity creation to modules and activities
@@ -30,6 +43,12 @@
     return {
         init: function () {
             console.log('init started');
+
+            // Show message left after page load
+            alertbox = $('#alertbox');
+            if (alertbox.length && alertbox.text()) {
+                localAlert();
+            }
 
             // Attach listener to .modal-close-btn's so that when the button is pressed the modal dialog disappears
             $('body').on('click', '.modal-close-btn', function () {
@@ -63,6 +82,9 @@
                 });
             }
         },
+        showAlert: function (message) {
+            localAlert(message)
+        },
         initForm: function () {
             $.validator.unobtrusive.parse('#formEdit');
             $('#btnSave').click(function () {
@@ -71,6 +93,14 @@
                 }
             });
             $('#formEdit .timepicker').timepicker({ 'timeFormat': 'H:i', 'scrollDefault': 'now' });
+            var input = $('#formResult'), result = input.val(), data = result.split('|');
+            if (result) {
+                localAlert(data[1], data[0]);
+                if (data[0] === 'success') {
+                    // refresh tree
+                    //$('#modalContainer').modal('hide');
+                }
+            }
         }
 
     };
