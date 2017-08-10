51600f7ba507a1d5d4a80e27c6e47e8cf3049cbc
Peter Nilsson <incido@gmail.com>
2017-08-11 00:56:48 +0200

    Changed Course Delete to Partial

---
+++ BAPA-LMS/Models/CourseViewModels/CourseDeleteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BAPA_LMS.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAPA_LMS.Models.CourseViewModels
+{
+    public class CourseDeleteViewModel
+    {
+        public string Name { get; set; }
+        public static implicit operator CourseDeleteViewModel(Course model)
+        {
+            return new CourseDeleteViewModel
+            {
+				Name = model.Name
+            };
+        }
+    }
+}
